115b19d7653c71ee4df28648b154ce40057df240
Abdirahman Jama <abdirahman.jama13@gmai.com>
2023-10-05 22:17:02 -0500

    partially finished

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,7 +1,44 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+console.log('basket items', basket);
+console.log('expecting false since basket is empty by default');
 
+function addItem(item){
+    if(basket.length >= 1){
+        console.log('item was added to basket array', true);
+        }
+        else{
+            console.log('item was added to basket array', false);
+        }
+return basket.push(item) && true || false;
+// expecting outcome to be true once items are added, false before items are added.
+    }
+addItem('fruit',);
+console.log(basket);
+addItem('milk');
+console.log(basket);
+addItem('eggs');
+console.log(basket);
+
+
+
+
+
+
+
+
+
+// function listItems(){
+
+// }
+
+// function empty(){
+//     for(items in basket){
+
+//     }
+// }
 
 
 
